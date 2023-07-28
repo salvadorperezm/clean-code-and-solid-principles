6b44b2df136830610006eb6333420050aa2e060b
salvadorperezm <salvadorperezm115@gmail.com>
2023-07-27 19:05:55 -0600

    Clase 16: Ejercicio con funciones

---
+++ src/clean-code/03-functions.ts
@@ -0,0 +1,46 @@
+(() => {
+
+    function getMovieById( id: string ) {
+        console.log({ id });
+    }
+
+    function getMovieCastById( id: string ) {
+        console.log({ id });
+    }
+
+    function getActorBioById( id: string ) {
+        console.log({ id });
+    }
+    
+    interface Movie {
+        cast: string[];
+        description: string; 
+        rating: number; 
+        title: string; 
+    }
+
+    function createMovie({title, description, rating, cast}: Movie ) {
+        console.log({ title, description, rating, cast });
+    }
+
+    function checkFullName(fullName: string): boolean {
+        console.log({ fullName })
+        return true;
+    }
+
+    function createActor( fullName: string, birthDate: Date ): boolean {
+        
+        // tarea asincrona para verificar nombre
+        // ..
+        // ..
+        if ( checkFullName(fullName) ) return false;
+
+        console.log('Crear actor', birthDate);
+        return true;        
+
+    }
+
+    
+
+
+})();
