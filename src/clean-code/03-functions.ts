d2de4b76bc2228648229e3a61906e80f6fde9367
salvadorperezm <salvadorperezm115@gmail.com>
2023-07-27 19:24:01 -0600

    Clase 17: Detalles adicionales sobre funciones

--- src/clean-code/03-functions.ts
+++ src/clean-code/03-functions.ts
@@ -40,7 +40,12 @@
 
     }
 
-    
+    // Continuar
+    const getPayAmount = ({ isDead = false, isSeparated = true, isRetired = false }) => {
 
+        if ( isDead ) return 1500;
+        if ( isSeparated ) return 2500;
+        return isRetired ? 3000 : 4000;
+    }
 
 })();
